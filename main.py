1d4191de28a0fd5a440bcd8f36d94ac87a587c3d
KinhNguyenVan <kinh30072004k@gmail.com>
2025-08-30 20:54:46 +0700

    pla

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ from src.search.model import CLIPModel
 from src.search.qdrant_db import qdrant_client
 
 embedding = CLIPModel()
-text = """Một người đàn ông mặc vest xanh, 
-áo sơ mi trắng và cà vạt đỏ đang đứng trước bảng trắng/khung 
-nền có nhiều công thức toán học"""
+text = """A chef puts fish into a white bowl at the exact moment the last fish falls from the plate, then pours flour into the bowl of fish with chopsticks touching the fish for the first time to mix, and finally tests the temperature of hot oil with chopsticks at the moment they are first lifted out of the oil.
+"""
 
 text_features = embedding.encode_text(text)
 search_result = qdrant_client.search(
     collection_name="image_clip_vectors",
-    query_vector=text_features.tolist(),
-    limit=10
+    query_vector=text_features.squeeze().tolist(),
+    limit=20
 )
 for hit in search_result:
     print(hit.payload, hit.score) 
